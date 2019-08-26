19c968838f9a05fcd8a51f8e3a9cf2ea377044e1
Riki Singh Khorana <rskhorana@gmail.com>
2019-08-26 12:56:31 -0400

    Fix copy to clipboard function

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,31 @@
-function copyLink(link) {
-  const copyText = `jsa.life/${link}`;
-  const listener = (e) => {
-    e.clipboardData.setData('text/plain' , copyText);
-    e.preventDefault();
-    document.removeEventListener('copy', listener);
-  };
+function createInput(link) {
+  const textArea = document.createElement('input');
+  textArea.setAttribute('value', link);
+  textArea.setAttribute('id', 'shortened-url');
+  textArea.setAttribute('class', 'hidden');
+  const div = document.getElementById('forCopy');
+  div.appendChild(textArea);
+}
 
-  document.addEventListener('copy' , listener);
+function copyInputContents() {
+  const copyText = document.getElementById('shortened-url');
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
   document.execCommand('copy');
+}
+
+function removeInput() {
+  const parent = document.getElementById('forCopy');
+  const input = document.getElementById('shortened-url');
+  parent.removeChild(input);
+}
+
+/**
+ * Copies {link} to clipboard
+ */
+function copyLink(link) {
+  createInput(link);
+  copyInputContents();
+  removeInput();
   M.toast({html: 'Link copied!'});
 }
