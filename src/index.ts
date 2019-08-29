27bfc4dee95f72b3e28e76f0f5771276bb33be04
Riki Singh Khorana <rskhorana@gmail.com>
2019-08-29 08:08:24 -0400

    Make favicon accessible on root of site

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,7 @@ setInterval(() => {
  * Middleware
  */
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../favicon')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({
