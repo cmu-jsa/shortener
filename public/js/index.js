266e38e6d2a1878032f9e8cf3c04727007260eac
Riki Khorana <riki.singh.khorana@sony.com>
2020-09-19 23:40:35 +0900

    Simplify copy link feature

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,4 @@
-function createInput(link) {
-  const textArea = document.createElement('input');
-  textArea.setAttribute('value', link);
-  textArea.setAttribute('id', 'shortened-url');
-  textArea.setAttribute('class', 'hidden');
-  const div = document.getElementById('forCopy');
-  div.appendChild(textArea);
-}
-
-function copyInputContents() {
-  const copyText = document.getElementById('shortened-url');
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  document.execCommand('copy');
-}
-
-function removeInput() {
-  const parent = document.getElementById('forCopy');
-  const input = document.getElementById('shortened-url');
-  parent.removeChild(input);
-}
-
-/**
- * Copies {link} to clipboard
- */
-function copyLink(link) {
-  createInput(link);
-  copyInputContents();
-  removeInput();
-  M.toast({html: 'Link copied!'});
-}
+document.getElementById('copyButton').addEventListener('click', (e) => {
+  navigator.clipboard.writeText(e.target.innerText);
+  M.toast({ html: 'Link copied!' });
+});
